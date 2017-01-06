8e27b07dbf5d8c77a487b50dd739e1831535be95
conradkun <conrichu@gmail.com>
2017-01-06 20:39:34 +0100

    Creation of UE project
    Initial commit.

---
+++ RoomScape/Source/RoomScape/RoomScapeGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RoomScape.h"
+#include "RoomScapeGameModeBase.h"
+
+
+
+
---
+++ RoomScape/Source/RoomScape/RoomScapeGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "RoomScapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMSCAPE_API ARoomScapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
